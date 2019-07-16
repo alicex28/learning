d6008cc5ca23630f8b17fc44af7363c5958248ba
Alice Chen <alicechen125@gmail.com>
2019-07-16 19:47:45 -0400

    changed exercise 99 to say hello

--- src/Exercise99.java
+++ src/Exercise99.java
@@ -1,1 +1,23 @@
-//hahahahaha im ruining ur project
+import java.util.Scanner;
+
+public class Exercise99 {
+
+    public static void main(String[] args) {
+
+
+        //hello
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
+
+
