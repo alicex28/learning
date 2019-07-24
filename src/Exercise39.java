9b3467a554f15c96076f86d2da3071b3113a3482
dchen <chendavid96@gmail.com>
2019-07-23 22:39:23 -0400

    explained exercise 39

--- src/Exercise39.java
+++ src/Exercise39.java
@@ -1,40 +1,19 @@
 public class Exercise39 {
 
-    /*39.1
-    public static void stars(int amount) {
+    private static void stars(int amount) {
         System.out.print("*");
     }
 
-    private static void printStars(int amount) {
-        int i = 0;
+    // Exercise 39.1
+    public static void printStars(int amount) {
+        int i = 1;
         while (i <= amount) {
             stars(amount);
             i = i+1;
         }
     }
 
-    public static void main(String[] args) {
-        printStars(5);
-        System.out.println();
-        printStars(3);
-        System.out.println();
-        printStars(9);
-    }
-    */
-
-    /*39.2
-    public static void stars(int amount) {
-        System.out.print("*");
-    }
-
-    private static void printStars(int amount) {
-        int i = 0;
-        while (i < amount) {
-            stars(amount);
-            i = i+1;
-        }
-    }
-
+    // Exercise 39.2
     public static void printSquare(int sideSize) {
         int i = 0;
         while (i < sideSize) {
@@ -45,27 +24,8 @@ public class Exercise39 {
 
     }
 
-    public static void main(String[] args) {
-        printSquare(4);
-
-    }
-
-     */
-
-    /*39.3
-    public static void stars(int amount) {
-        System.out.print("*");
-    }
-
-    private static void printStars(int amount) {
-        int i = 0;
-        while (i < amount) {
-            stars(amount);
-            i = i+1;
-        }
-    }
-
-    public static void printRectangle(int width, int height) {
+    // Exercise 39.3
+    private static void printRectangle(int width, int height) {
         int i = 0;
         while (i < height) {
             printStars(width);
@@ -75,26 +35,8 @@ public class Exercise39 {
 
     }
 
-    public static void main(String[] args) {
-        printRectangle(17,3);
-
-    }
-    */
-
-    //39.4
-    public static void stars(int amount) {
-        System.out.print("*");
-    }
-
-    private static void printStars(int amount) {
-        int i = 0;
-        while (i < amount) {
-            stars(amount);
-            i = i+1;
-        }
-    }
-
-    public static void printTriangle(int size) {
+    // Exercise 39.4
+    private static void printTriangle(int size) {
         int i = 1;
         while (i <= size) {
             printStars(i);
@@ -105,9 +47,11 @@ public class Exercise39 {
     }
 
     public static void main(String[] args) {
-        printTriangle(4);
+        //printStars(0);
+        //printSquare(4);
+        //printRectangle(3, 5);
+        //printTriangle(4);
     }
-
 
 }
 
